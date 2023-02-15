c4e9fbbdfe381d31b1c2097b67dc82f7c97af0d3
Giulia <giuliac35@gmail.com>
2023-02-15 16:21:58 +0100

    Create BufferedReader object

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Start {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            try {
+                System.out.println("Enter the dividend: ");
+                int dividend = Integer.parseInt(reader.readLine());
+                System.out.print("Enter the divisor: ");
+                int divisor = Integer.parseInt(reader.readLine());
+                int division = dividend / divisor;
+                System.out.println(division);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 }
